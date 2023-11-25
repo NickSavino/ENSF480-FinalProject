4e2a02a1976613d3fdb34f23da4793d7b419d855
Liam Mah <liam.mah@ucalgary.ca>
2023-11-25 15:50:12 -0700

    Adding base for AuthenticationController.

--- FlightReservationSystem/src/flightapp/controllers/AuthenticationController.java
+++ FlightReservationSystem/src/flightapp/controllers/AuthenticationController.java
@@ -13,4 +13,29 @@ public class AuthenticationController {
     public AuthenticationController(Airline airline) {
         this.airline = airline;
     }
+
+    public void login(int customerId, String password)
+    {
+        // TODO: Implement
+        // Need to authenticate via Singleton
+    }
+
+    public void signup(String firstName, String lastName, String email, int age, String password, String street, 
+        String city, String province, String country, int newCustomerId)
+    {
+        // TODO: Implement
+        // Remember to add all information to the Singleton instance
+        // Need to make sure that newCustomerId does not already exist
+    {
+
+    public void becomeAirlineMember()
+    {
+        // TODO: Implement
+    }
+
+    public applyForAirlineCreditCard(int newCreditCardNumber, int newSecurityCode, RegisteredCustomer customer)
+    {
+        // TODO: Implement
+    }
+
 }

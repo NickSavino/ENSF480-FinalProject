517c175db83088e6c7f2398d0008bc50e799ad56
Liam Mah <liam.mah@ucalgary.ca>
2023-11-27 11:28:12 -0700

    Adding tables.

--- flightdatabase.sql
+++ flightdatabase.sql
@@ -54,6 +54,27 @@ CREATE TABLE EMPLOYEES (
 -- 2.16 Field for email
 -- 2.17 Add foreign key referencing PurchaseId
 
+DROP TABLE IF EXISTS CUSTOMERS
+CREATE TABLE CUSTOMERS (
+      customerId          INT,
+      status              VARCHAR(25),
+      username            VARCHAR(25),
+      password            VARCHAR(25),
+      creditCardNumber    VARCHAR(25),
+      creditCardSecurityCode INT,
+      isAirlineMember     BOOLEAN,
+      companionVoucherUsable BOOLEAN,
+      firstName           VARCHAR(25),
+      lastName            VARCHAR(25),
+      houseNumber         VARCHAR(25),
+      street              VARCHAR(25),
+      city                VARCHAR(25),
+      province            VARCHAR(25),
+      country             VARCHAR(25),
+      email               VARCHAR(25),
+      purchaseId          INT,
+      PRIMARY KEY (customerId)
+);
 
 -- 3. Add table for aircrafts
 -- 3.1 Field for aircraftId
@@ -63,9 +84,27 @@ CREATE TABLE EMPLOYEES (
 -- 3.5 Field for amount of comfort seats
 -- 3.6 Field for total amount of seats
 
+DROP TABLE IF EXISTS AIRCRAFTS
+CREATE TABLE AIRCRAFTS (
+      aircraftId          INT,
+      aircraftModel       VARCHAR(25),
+      ordinarySeats       INT,
+      businessSeats       INT,
+      comfortSeats        INT,
+      totalSeats          INT,
+      PRIMARY KEY (aircraftId)
+);
+
 -- 4. Add table for locations
 -- 4.1 Add field for location name (String)
 -- 4.2 Add field for locationID (String; e.g. YYC, YVR, etc.)
+
+DROP TABLE IF EXISTS LOCATIONS
+CREATE TABLE LOCATIONS (
+      locationId          INT,
+      locationName        VARCHAR(25),
+      PRIMARY KEY (locationId)
+);
 
 -- 5. Add table for purchases
 -- 5.1 Add field for purchaseId (String)
@@ -77,6 +116,18 @@ CREATE TABLE EMPLOYEES (
 -- 5.7 Add field for items purchased (String)
 -- 5.8 Add foreign key reference for ticketId in tickets table
 
+DROP TABLE IF EXISTS PURCHASES
+CREATE TABLES (
+      purchaseId          INT,
+      loungeAccess        BOOLEAN,
+      creditCardNumber    VARCHAR(25),
+      creditCardSecurityCode INT,
+      totalPurchaseCost   INT,
+      ticketInsurance     BOOLEAN,
+      itemsPurchased      VARCHAR(25),
+      ticketId            INT,
+      PRIMARY KEY (purchaseId)
+);
 
 -- 6. Add table for tickets
 -- 6.1 Add field for ticketId (String)
@@ -87,6 +138,19 @@ CREATE TABLE EMPLOYEES (
 -- 6.6 Add field for flight year (int)
 -- 6.7 Add field for flight hour (int)
 -- 6.8 Add field for flight minute (int)
+
+DROP TABLE IF EXISTS TICKETS
+CREATE TABLE TICKETS (
+      ticketId            INT,
+      seatNumber          INT,
+      flightNumber        INT,
+      flightDay           INT,
+      flightMonth         INT,
+      flightYear          INT,
+      flightHour          INT,
+      flightMinute        INT,
+      PRIMARY KEY (ticketId)
+);
 
 -- 8. Add table for flights
 -- 8.1 Add field for flightId (int)
@@ -102,12 +166,39 @@ CREATE TABLE EMPLOYEES (
 -- 8.11 Add field for flight departure hour
 -- 8.12 Add field for flight departure minute
 
+DROP TABLE IF EXISTS FLIGHTSEATS
+CREATE TABLE FLIGHTS (
+      flightId            INT,
+      aircraftId          INT,
+      originId            INT,
+      destinationId       INT,
+      flightDuration      INT,
+      flightCrewId        INT,
+      baseFlightCost      INT,
+      flightDepartureMonth INT,
+      flightDepartureDay  INT,
+      flightDepartureYear INT,
+      flightDepartureHour INT,
+      flightDepartureMinute INT,
+      PRIMARY KEY (flightId)
+);
+
 -- 10. Add table for flights and seats (PK is flightId and seatId)
 -- 10.1 Add field for flightId (int)
 -- 10.2 Add field for seatId (int)
 -- 10.3 Add field for seatIsBooked (boolean)
 -- 10.4 Add field for seatType (String)
 -- 10.5 Add field for passengerId
+
+DROP TABLE IF EXISTS FLIGHTSEATS
+CREATE TABLE FLIGHTSEATS (
+      flightId            INT,
+      seatId              INT,
+      seatIsBooked        BOOLEAN,
+      seatType            VARCHAR(25),
+      passengerId         INT,
+      PRIMARY KEY (flightId, seatId)
+);
 
 
 
